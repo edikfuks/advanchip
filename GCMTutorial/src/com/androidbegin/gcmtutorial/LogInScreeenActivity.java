9477659dacea08a8b40cd907303e7879a9dd8019
Edward <fuks@usc.edu>
2013-11-22 01:01:03 -0800

    New commit

--- GCMTutorial/src/com/androidbegin/gcmtutorial/LogInScreeenActivity.java
+++ GCMTutorial/src/com/androidbegin/gcmtutorial/LogInScreeenActivity.java
@@ -11,7 +11,7 @@ public class LogInScreeenActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		setContentView(R.layout.homescreen);lmlmldcdcddcd
+		setContentView(R.layout.homescreen);
 		
 		
 		
